5d25d059e8278b25253319c08452890b65b64d47
Caio de Souza <caiodesouza.cds@gmail.com>
2024-06-15 15:18:58 -0300

    Testando apenas

--- Dev_Sistema/index.py
+++ Dev_Sistema/index.py
@@ -1,1 +1,8 @@
-qualquer = int(input("blablabla:"))
+#ESTAREI TESTANDO
+
+num1 = int(input("Digite um numero"))
+num2= int(input("Digite outro numero"))
+
+soma = num1 + num2
+
+print("A soma dos numeros digitados é: " , soma)
