4eccec0dd81569f2d87d651dcd1352df90726d92
CARLOS ALEXANDRE MONTEIRO DERETTI <carlos.deretti@univille.br>
2024-06-19 20:21:05 -0300

    Testando

--- votacao.py
+++ votacao.py
@@ -1,2 +1,20 @@
-print("Ola Wardo")
-print("Uaaaa")
+#prefeitos / numero do candidato 
+joão = 111
+paulo = 222
+pedro = 333
+junior_do_povão = 444
+
+# vereador / numero do candidato 
+papa_capim = 1
+efraim = 2
+r = 3
+e = 4
+a = 5
+d = 6
+o = 7 
+r = 8
+vv = 9
+ee = 10
+
+while continuar != 0 :
+  print(para começar a eleição digite y)
