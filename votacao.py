b23a4cc192ddaca8c7480418d987728a7b39eb8f
CARLOS ALEXANDRE MONTEIRO DERETTI <carlos.deretti@univille.br>
2024-06-21 21:27:32 -0300

    AAAA2

---
+++ votacao.py
@@ -0,0 +1,22 @@
+#prefeitos / numero do candidato 
+joão = 111
+paulo = 222
+pedro = 333
+junior_do_povão = 444
+
+# vereador / numero do candidato 
+papa_capim = 1
+efraim = 2
+reinaldo = 3
+ednaldo = 4
+ana = 5
+dioga = 6
+oranja = 7 
+reginaldo = 8
+valoranta = 9
+dburcio = 10
+
+while continuar != 0 :
+  inicio = input("Responda com Y para iniciar o sistema").upper()
+  
+
